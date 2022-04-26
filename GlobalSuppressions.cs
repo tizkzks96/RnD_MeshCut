85878bd8e9c0d15a7eaf5202e24d869caf7004b3
ChoiSeungho <tizkzks96@naver.com>
2022-04-26 10:59:54 +0900

    씬 수정

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "<보류 중>", Scope = "member", Target = "~M:MeshManager.Start")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "<보류 중>", Scope = "member", Target = "~M:MeshManager.Update")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:사용되지 않는 private 멤버 제거", Justification = "<보류 중>", Scope = "member", Target = "~M:MeshManager.OnDrawGizmos")]
